aa52341484495dcb4083b0f1b456e2e5898d0da2
anand mule <anand.mule@hcl.com>
2021-06-06 21:47:44 +0530

    Adding jenkins

--- Framework/src/test/java/com/framework/pages/BaseClass.java
+++ Framework/src/test/java/com/framework/pages/BaseClass.java
@@ -8,16 +8,18 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.beust.jcommander.Parameter;
 import com.framework.utilities.BrowserFactory;
 import com.framework.utilities.ConfigDataProvider;
 import com.framework.utilities.ExcelDataProvider;
@@ -48,12 +50,16 @@ public class BaseClass {
 		 
 	}
 	
-	
+	@Parameters({"getBrowserName","getAppURL"})
 	@BeforeClass
-	public void setup() {
-		driver =BrowserFactory.startApplication(driver, configDataProvider.getBrowser(), 
-				configDataProvider.getTestUrl());
+	public void setup(String getBrowserName,String getAppURL) {
+		//driver =BrowserFactory.startApplication(driver, configDataProvider.getBrowser(), configDataProvider.getTestUrl());
+		
+		driver =BrowserFactory.startApplication(driver,getBrowserName , getAppURL);
+		Reporter.log("Login on browser  "+getBrowserName +"with app url "+ getAppURL);
 	}
+	
+
 	
 	@AfterClass
 	public void tearDown() {
--- Framework/src/test/java/com/framework/utilities/BrowserFactory.java
+++ Framework/src/test/java/com/framework/utilities/BrowserFactory.java
@@ -7,12 +7,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
